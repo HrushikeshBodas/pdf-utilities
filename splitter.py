e244c49643e0b41b56088b2c15a943392fbf2a04
hybrid1999 <31095519+HrushikeshBodas@users.noreply.github.com>
2020-10-02 15:01:09 +0530

    Add splitter code
    Splits a 4 page in 1 pdf into individual pdfs

---
+++ splitter.py
@@ -0,0 +1,31 @@
+import os
+from pdf2image import convert_from_path
+from PIL import Image
+from fpdf import FPDF
+
+files = []
+for (roots, dirs, file_) in os.walk("."):
+    files = file_
+    break
+print("aa", files)
+for f in files:
+    if f.endswith(".pdf"):
+        new_pdf = FPDF()
+        new_images = []
+        images = convert_from_path("./{}".format(f))
+        for image in images:
+            w, h = image.size
+            midw = w / 2
+            midh = h / 2
+            new_images.append(image.crop((0, 0, w, midh)))
+            new_images.append(image.crop((0, midh, w, h)))
+        pdf_name = "split_{}".format(f)
+        new_images[0].save(
+            pdf_name,
+            "PDF",
+            resolution=100,
+            save_all=True,
+            append_images=new_images[1:],
+            optimize=True,
+            quality=80,
+        )
